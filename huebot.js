4c8dcdc5634fa7316f9821afbc5542e0e765f033
madprops/mc <manuelchaves@gmail.com>
2019-05-24 08:21:33 -0600

    Command Similarity

--- huebot.js
+++ huebot.js
@@ -1617,6 +1617,28 @@ function start_connection(room_id)
 				run_command(cmd, arg, data)
 			}
 
+			else
+			{
+				let highest_num = 0
+				let highest_cmd
+
+				for(let cmd2 in commands)
+				{
+					let num = string_similarity(cmd, cmd2)
+
+					if(num > highest_num)
+					{
+						highest_num = num
+						highest_cmd = cmd2
+					}
+				}
+
+				if(highest_num >= 0.8)
+				{
+					run_command(highest_cmd, arg, data)
+				}
+			}
+
 			return false
 		}
 
@@ -3552,5 +3574,71 @@ function start_connection(room_id)
 		{
 			process_command(obj)
 		}
+	}
+
+	function string_similarity(s1, s2) 
+	{
+		let longer = s1
+		let shorter = s2
+
+		if(s1.length < s2.length) 
+		{
+			longer = s2
+			shorter = s1
+		}
+
+		let longerLength = longer.length
+
+		if(longerLength == 0) 
+		{
+			return 1.0
+		}
+
+		return (longerLength - string_similarity_distance(longer, shorter)) / parseFloat(longerLength)
+	}
+
+	function string_similarity_distance(s1, s2) 
+	{
+		s1 = s1.toLowerCase()
+		s2 = s2.toLowerCase()
+	
+		let costs = new Array()
+
+		for(let i = 0; i <= s1.length; i++) 
+		{
+			let lastValue = i
+
+			for(let j = 0; j <= s2.length; j++) 
+			{
+				if(i == 0)
+				{
+					costs[j] = j
+				}
+
+				else 
+				{
+					if (j > 0) 
+					{
+						let newValue = costs[j - 1]
+
+						if(s1.charAt(i - 1) != s2.charAt(j - 1))
+						{
+							newValue = Math.min(Math.min(newValue, lastValue),
+							costs[j]) + 1
+						}
+
+						costs[j - 1] = lastValue
+						lastValue = newValue
+					}
+				}
+			}
+
+			if(i > 0)
+			{
+				costs[s2.length] = lastValue
+			}
+		}
+
+		return costs[s2.length]
 	}
 }
