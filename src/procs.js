890ba4b103f22addb4ae34acb70941652e61e53e
Auric Vente <mprops@protonmail.com>
2020-11-24 20:10:38 -0600

    qmod

--- src/procs.js
+++ src/procs.js
@@ -755,8 +755,8 @@ module.exports = function (Huebot) {
     Huebot.db.queue[args[0]].push(obj)
 
     Huebot.save_file("queue.json", Huebot.db.queue, function () {
-      let links = `[whisper ${Huebot.prefix}q play ${obj.id}]Play This[/whisper]`
-      links += ` | [whisper ${Huebot.prefix}q ${args[0]} next]Play Next[/whisper]`
+      let links = `[whisper ${Huebot.prefix}q play ${obj.id}]Play[/whisper]`
+      links += ` | [whisper ${Huebot.prefix}q ${args[0]} next]Next[/whisper]`
       links += ` | [whisper ${Huebot.prefix}q remove ${obj.id} $id$]Remove[/whisper]`
       let ans = `Done >> ${links}`
       Huebot.send_message(ox.ctx, ans)
